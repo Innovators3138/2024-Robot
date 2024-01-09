0fc11bd80ff2ce0db789a73e51a9fb809bc011f0
Michael Hill <michael.hill.10@gmail.com>
2024-01-08 20:55:04 -0500

    update gitignore

--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ import commands2
 
 import robotcontainer
 
+
 class MyRobot(commands2.TimedCommandRobot):
     """
     
@@ -13,31 +14,31 @@ class MyRobot(commands2.TimedCommandRobot):
         
         """
         self.container = robotcontainer.RobotContainer()
-    
+
     def disabledInit(self) -> None:
         """"""
-    
+
     def disabledPeriodic(self) -> None:
         """"""
-    
+
     def autonomousInit(self) -> None:
         """
         """
-    
+
     def autonomousPeriodic(self) -> None:
         """
         """
-    
+
     def teleopInit(self) -> None:
         """
         """
-    
+
     def teleopPeriodic(self) -> None:
         """"""
         wpilib.DataLogManager.start()
 
         wpilib.DriverStation.startDataLog(wpilib.DataLogManager.getLog())
-    
+
     def testInit(self) -> None:
         # Cancels all running commands at the start of test mode
         commands2.CommandScheduler.getInstance().cancelAll()
--- robotcontainer.py
+++ robotcontainer.py
@@ -1,14 +1,15 @@
 import constants
 
+
 class RobotContainer(object):
     """Container for the robot
     
     """
 
     def __init__(self):
-        #Do all subsystem inits here
-        self.configure_buttons
-    
+        # Do all subsystem inits here
+        self.configure_buttons()
+
     def configure_buttons(self):
-        #link up the button actions with commands here
+        # link up the button actions with commands here
         pass
